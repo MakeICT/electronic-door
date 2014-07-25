0728eb6f373caf7ff858c4243c622b446b2c957e
Christian Kindel <iceman81292@gmail.com>
2014-07-25 19:33:36 +0000

    Removed software PWM code

--- rpi.py
+++ rpi.py
@@ -31,9 +31,6 @@ class InterfaceControl(object):
 		GPIO.setup(self.GPIOS['unlock_LED'], GPIO.OUT)
 		GPIO.setup(self.GPIOS['power_LED'], GPIO.OUT)
 		
-		#Set up Software PWM
-		#GPIO.setup(self.GPIOS['buzzer'], GPIO.OUT)
-
 		#Set up Hardware PWM - Only works on GPIO 18
 		wiringpi2.wiringPiSetupGpio()  
 		wiringpi2.pinMode(self.GPIOS['buzzer'], 2)      # set pin to PWM mode
@@ -42,6 +39,7 @@ class InterfaceControl(object):
 
 		GPIO.setup(self.GPIOS['doorStatus1'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		GPIO.setup(self.GPIOS['doorStatus2'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		
 		#For testing: remove before pull request
 		GPIO.setup(27, GPIO.OUT)
 		GPIO.output(27,False)
@@ -50,8 +48,6 @@ class InterfaceControl(object):
                 #end test code
 
 		GPIO.setwarnings(True)
-
-#		self.buzzer = GPIO.PWM(self.GPIOS['buzzer'], 360)
 
 		#For testing: remove before pull request
 		GPIO.setup(27, GPIO.OUT)
@@ -93,16 +89,8 @@ class InterfaceControl(object):
 		'''
 
 		if buzzerOn:
-			#software PWM
-			#self.buzzer.start(50)
-
-			#hardware PWM
 			wiringpi2.pwmWrite(self.GPIOS['buzzer'], 30)    # 30% duty cycle
 		else:
-			#software PWM
-			#self.buzzer.stop()
-
-			#hardware PWM
 			wiringpi2.pwmWrite(self.GPIOS['buzzer'], 0)
 
 
