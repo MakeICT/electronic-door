9d6dc2714e817305ff5a9b2ba5c7c0fa4ddcab74
Christian Kindel <iceman81292@gmail.com>
2014-08-11 21:04:21 +0000

    Fix issue with history and colored prompt

--- database_cli.py
+++ database_cli.py
@@ -23,7 +23,7 @@ from rm_user import rmUser
 class DatabaseCLI(Cmd):
 	def __init__(self,completekey='tab', stdin=None, stdout=None):
 		Cmd.__init__(self,completekey, stdin, stdout)
-		self.prompt = "\033[1m\033[34mDB_CMD>\033[0m "
+		self.prompt = "\001\033[1m\033[34m\002DB_CMD>\001\033[0m\002 "
 
 	def emptyline(self):
 		pass 	
@@ -75,3 +75,4 @@ class DatabaseCLI(Cmd):
 			Cmd.cmdloop(self, intro)
 		except KeyboardInterrupt:
 			print "\nCaught Ctrl+C, exiting"
+
