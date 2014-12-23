0db8c15f0e16b852e11997cbc3dc09d421b28b62
Dominic Canare <dom@greenlightgo.org>
2014-12-22 20:54:35 -0600

    add override with reason

--- python/override.py
+++ python/override.py
@@ -15,12 +15,17 @@ if __name__ == "__main__":
 	import argparse
 	
 	parser = argparse.ArgumentParser(description='Manually control the door latch')
+	parser.add_argument('reason', metavar='reason', nargs='*', help="Reason why this is happening")
 	parser.add_argument("-l", "--lock", help="Lock the door", action="store_true")
 	args = parser.parse_args()
+	args.reason = ' '.join(args.reason)
+
 	try:
 		from enroll import killDoorLock, startDoorLock
+		from backend import backend
 		import rpi
 		if not args.lock:
+			backend.log('message', message='Unlock door: %s' % args.reason)
 			killDoorLock()
 			rpi.interfaceControl.unlockDoor()
 	except KeyboardInterrupt:
--- web/www/admin.php
+++ web/www/admin.php
@@ -12,17 +12,41 @@
 	require_once('../include/Backend.php');
 
 	if(empty($_SESSION['userID'])){
-		$_SESSION['redirectLocation'] = 'logViewer.php';
+		$_SESSION['redirectLocation'] = 'admin.php';
 		header("Location: login.php");
 		exit();
 	}
 	 
+	if(!empty($_REQUEST)){
+		if($_REQUEST['action'] == 'Unlock'){
+			if(!empty($_REQUEST['reason'])){
+				$reason = addslashes($_REQUEST['reason']);
+				exec("sudo /home/pi/code/makeictelectronicdoor/python/override.py \"$reason\" > /tmp/overrideStatus 2>&1 &");
+				sleep(1);
+				$_SESSION['messages'][] = 'Unlock message sent...<hr/><pre>' . implode('<br/>', file('/tmp/overrideStatus')) . '</pre>';
+				header("Location: $_SERVER[PHP_SELF]");
+				exit();
+			}else{
+				$_SESSION['errors'][] = 'You must specify a reason!';
+			}
+		}
+	}
+
 	include_once("../include/BasicTemplate.php");
 	$template = new BasicTemplate(file_get_contents("template.html"));
 	$template->bufferStart();
+
+	echo getFormattedErrors();
+	echo getFormattedMessages();
+
 ?>
 
-
-Test page
+<h2>Override door lock</h2>
+<form method="POST">
+	<h3>Reason:</h3>
+	<textarea name="reason"></textarea>
+	<br/>
+	<input type="submit" name="action" value="Unlock" />
+</form>
 
 <?php $template->bufferStop('PAGE_CONTENT'); ?>
