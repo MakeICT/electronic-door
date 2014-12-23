837d0597356b3ab3da3def433a5da8380cef78f6
Dominic Canare <dom@greenlightgo.org>
2014-12-22 22:01:26 -0600

    done testing

--- python/override.py
+++ python/override.py
@@ -11,26 +11,35 @@ Authors:
 	Christian Kindel <iceman81292@gmail.com>
 '''
 
+def doUnlock(reason):
+	needToRestart = False
+	try:
+		import enroll
+		from backend import backend
+
+		import time
+
+		backend.log('message', message='Unlock door: %s' % args.reason)
+		if enroll.killDoorLock() != 1:
+			needToRestart = True
+
+		import rpi
+		rpi.interfaceControl.unlockDoor()
+	except KeyboardInterrupt:
+		pass
+	finally:
+		if needToRestart:
+			print("Locking door back")
+			enroll.startDoorLock()
+		
+
+
 if __name__ == "__main__":
 	import argparse
 	
 	parser = argparse.ArgumentParser(description='Manually control the door latch')
-	parser.add_argument('reason', metavar='reason', nargs='*', help="Reason why this is happening")
-	parser.add_argument("-l", "--lock", help="Lock the door", action="store_true")
+	parser.add_argument('reason', metavar='reason', nargs='+', help="Reason why this is happening")
 	args = parser.parse_args()
 	args.reason = ' '.join(args.reason)
 
-	try:
-		from enroll import killDoorLock, startDoorLock
-		from backend import backend
-		import rpi
-		if not args.lock:
-			backend.log('message', message='Unlock door: %s' % args.reason)
-			killDoorLock()
-			rpi.interfaceControl.unlockDoor()
-	except KeyboardInterrupt:
-		pass
-	finally:
-		startDoorLock()
-		
-
+	doUnlock(args.reason)
