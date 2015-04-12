f10cbbc1afa3fbf8b389ee8f9fca491a5cf06fad
Christian Kindel <iceman81292@gmail.com>
2015-04-12 13:35:24 -0500

    sql backup

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@
 
 sudo sed -i '$ a\0  1    * * *   www-data        php -f /home/pi/code/makeictelectronicdoor/web/include/waUpdater.php' /etc/crontab
 
+sudo sed -i '$ a\0  1    * * *   www-data        /home/pi/code/makeictelectronicdoor/backup-database.sh' /etc/crontab
+
 sudo ln -s ~/code/makeictelectronicdoor/door-lock.conf /etc/init/door-lock.conf
+sudo ln -s ~/code/makeictelectronicdoor/alerter/alarm-alerter.conf /etc/init/alarm-alerter.conf
