f22213679180e4355f012a404015330a002b6ba9
Dominic Canare <dom@makeict.org>
2015-10-27 01:29:00 -0500

    create basic plugin architecture

---
+++ server/software/plugins/wildApricotSync/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+	name: 'wildApricotSync',
+	
+	options: {
+		'API key': 'text',
+	},
+
+	actions: {
+		'Sync Now': function(){
+			console.log('Not really syncing...');
+		},
+	},
+
+	onInstall: function(){
+		
+	},
+
+	onUninstall: function(){
+		
+	},
+};
