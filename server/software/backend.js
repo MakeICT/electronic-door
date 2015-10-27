41991edf805380784f07c468a621280e13364254
Dominic Canare <dom@makeict.org>
2015-10-27 01:30:12 -0500

    create basic plugin architecture

--- server/software/backend.js
+++ server/software/backend.js
@@ -3,10 +3,8 @@
  *  Description: obstruction to the database.
  **/ 
 
-
 var fs = require('fs');
 var pg = require('pg');
-
 
 var credentials = fs.readFileSync('DB_CREDENTIALS').toString().trim().split('\t');
 var connectionParameters = {
@@ -21,20 +19,20 @@ function query(sql, params, onSuccess, onError, keepOpen){
 		if(err) {
 			return console.error('Failed to connect', err);
 		}
-		client.query(sql, params, function(err, result) {
+		return client.query(sql, params, function(err, result) {
 			if(!keepOpen){
 				done();
 			}
 			
 			if(err){
+				console.error('Error executing query', err);
+				console.error(sql);
 				if(onError){
-					onError(err);
-				}else{
-					console.error('Error executing query', err);
+					return onError(err);
 				}
 			}else{
 				if(onSuccess){
-					onSuccess(result.rows, done);
+					return onSuccess(result.rows, done);
 				}
 			}
 		});
@@ -42,6 +40,14 @@ function query(sql, params, onSuccess, onError, keepOpen){
 }
 
 module.exports = {
+	regroup: function(array, keyName, valueName){
+		var data = {};
+		for(var i=0; i<array.length; i++){
+			data[array[i][keyName]] = array[i][valueName];
+		}
+		return data;
+	},
+	
 	getUsers: function(q, isAdmin, keyActive, joinDate, onSuccess, onFailure) {
 		var sql =
 			'SELECT ' +
@@ -82,5 +88,107 @@ module.exports = {
 		var params = [user.email, user.firstName, user.lastName, user.joinDate];
 		
 		return query(sql, params, onSuccess, onFailure);
+	},
+		
+	enablePlugin: function(pluginName, onSuccess, onFailure){
+		return query('UPDATE plugins SET enabled = TRUE WHERE name = $1', [pluginName], onSuccess, onFailure);
+	},
+	
+	disablePlugin: function(pluginName, onSuccess, onFailure){			
+		return query('UPDATE plugins SET enabled = FALSE WHERE name = $1', [pluginName], onSuccess, onFailure);
+	},
+	
+	registerPlugin: function(plugin, onSuccess, onError){
+		return query(
+			'INSERT INTO plugins (name) VALUES ($1)',
+			[plugin.name],
+			function(){
+				return query(
+					'SELECT "pluginID" FROM plugins WHERE name = $1',
+					[plugin.name],
+					function(rows){
+						var pluginID = rows[0].pluginID;
+						var sql = 'INSERT INTO "pluginOptions" (name, type, ordinal, "pluginID") VALUES ';
+						var ordinal = 0;
+						var params = [];
+						for(var key in plugin.options){
+							sql += '($' + (ordinal*4+1) + ', $' + (ordinal*4+2) + ', $' + (ordinal*4+3) + ', $' + (ordinal*4+4) + '), ';
+							params.push(key);
+							params.push(plugin.options[key]);
+							params.push(ordinal);
+							params.push(pluginID);
+							
+							ordinal++;
+						}
+						if(params.length > 0){
+							sql = sql.substring(0, sql.length-2);
+							return query(sql, params, onSuccess, onError);
+						}
+					},
+					onError
+				);
+			},
+			onError
+		);
+	},
+
+	getPlugins: function(onSuccess, onFailure){
+		return query('SELECT * FROM plugins', null, onSuccess, onFailure);
+	},
+	
+	getPluginOptions: function(pluginName, onSuccess, onFailure){
+		var sql = 
+			'SELECT ' +
+			'	"pluginOptions"."name", ' +
+			'	type, ' +
+			'	value ' +
+			'FROM plugins ' +
+			'	JOIN "pluginOptions" ON plugins."pluginID" = "pluginOptions"."pluginID" ' +
+			'	LEFT JOIN "pluginOptionValues" ON "pluginOptions"."pluginOptionID" = "pluginOptionValues"."pluginOptionID" ' +
+			'WHERE plugins.name = $1 ' +
+			'ORDER BY ordinal ';
+		
+		var self = this;
+		var preProcess = function(data){
+			onSuccess(self.regroup(data, 'name', 'value'));
+		};
+		
+		return query(sql, [pluginName], onSuccess, onFailure);
+	},
+	
+	setPluginOption: function(pluginName, optionName, value, onSuccess, onError){
+		return query(
+			'SELECT "pluginID" FROM plugins WHERE name = $1',
+			[pluginName],
+			function(plugin){
+				var pluginID = plugin[0].pluginID;
+				
+				return query(
+					'SELECT "pluginOptionID" FROM "pluginOptions" WHERE "pluginID" = $1 AND name = $2',
+					[pluginID, optionName],
+					function(pluginOption){
+						var optionID = pluginOption[0].pluginOptionID;
+						return query(
+							'SELECT value FROM "pluginOptionValues" WHERE "pluginOptionID" = $1',
+							[optionID],
+							function(rows){
+								return query(
+									'DELETE FROM "pluginOptionValues" WHERE "pluginOptionID" = $1',
+									[optionID],
+									function(){
+										var sql = 'INSERT INTO "pluginOptionValues" ("pluginOptionID", value) VALUES ($1, $2)';
+										return query(sql, [optionID, value], onSuccess, onError);
+									},
+									onError
+								);
+							},
+							onError
+						);
+					},
+					onError
+				);
+			},
+			onError
+		);
 	}
 };
