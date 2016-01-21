bab1a46550c76f9b260173feff052b053e7fd977
Dominic Canare <dom@makeict.org>
2016-01-20 20:32:51 -0600

    clean up old crap

--- server/software/vista/arm-away.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Sending arm away..."
-
-/home/pi/code/makeictelectronicdoor/vista/send-code.sh 2
-/home/pi/code/makeictelectronicdoor/vista/send-code.sh 2
-
-exit $?
-
--- server/software/vista/disarm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Sending disarm..."
-
-/home/pi/code/makeictelectronicdoor/vista/send-code.sh 1
-/home/pi/code/makeictelectronicdoor/vista/send-code.sh 1
-
-exit $?
-
