115c4ddebfa244b2953a19911cfdbf5a1872885d
Dominic Canare <dom@makeict.org>
2016-01-26 23:10:44 -0600

    node serial testing

--- server/software/plugins/mcp-plugin-super-serial/test.js
+++ server/software/plugins/mcp-plugin-super-serial/test.js
@@ -1,21 +1,22 @@
 var SerialPort = require("serialport");
 
 serialPort = new SerialPort.SerialPort(
-	'/dev/ttyAMA0', { baudrate: 9600},
+	'/dev/pts/3', { baudrate: 9600},
 	false
 );
-serialPort.open(
-	function(error){
-		if(error){
-			console.log(error);
-		}else{
-			console.log('connected');
-			serialPort.write('hi');
-			console.log('said hi');
-		}
+serialPort.on('open', function(error){
+	if(error){
+		console.log(error);
+	}else{
+		console.log('connected');
+		serialPort.write('hi');
+		console.log('said hi');
 	}
-);
+});
 
 serialPort.on('data', function(data){
 	console.log('data received: ' + data);
 });
+
+
+serialPort.open();
