b0b350d8889274d6f4bb5544a494e7794c352912
Christian Kindel <iceman81292@gmail.com>
2016-02-10 00:52:30 -0600

    remove debug

--- server/software/plugins/mcp-plugin-door-unlocker/index.js
+++ server/software/plugins/mcp-plugin-door-unlocker/index.js
@@ -100,11 +100,8 @@ module.exports = {
 	
 	receiveMessage: function(source, messageID, data){
 		if(messageID == 'serial-data-received'){
-			backend.debug('door unlocker received some serial data');
 			if(data['to'] == 0){
-				backend.debug('it is for the master');
 				if(data['function'] == NFC){
-					backend.debug('and it is a request to unlock a door');
 					var client = backend.getClientByID(data['from']);
 					var options = backend.regroup(client.plugins[module.exports.name].options, 'name', 'value');
 					
