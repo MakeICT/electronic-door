37642e8339b5cb1a72dec2965e853a2f61c03f76
Dominic Canare <dom@makeict.org>
2016-03-25 03:05:41 -0500

    add service

--- server/software/setup.sh
+++ server/software/setup.sh
@@ -44,12 +44,39 @@ fi
 #############################
 # HTTPS cert and key
 #############################
-read -p "Generate new SSL cert and key [y/N]: " response
-if [ "Y" = "$response" ] || [ "y" = "$response" ]; then
+read -p "Generate new SSL cert and key [Y/n]: " response
+if [ "" = "$response" ] || [ "Y" = "$response" ] || [ "y" = "$response" ]; then
 	mkdir credentials >/dev/null 2>&1
 	subj="/CN=mcp.makeict.org/O=MakeICT/C=US/ST=Kansas/L=Wichita/OU=IT"
 
 	openssl req -x509 -newkey rsa:2048 -keyout credentials/key.pem -out credentials/cert.pem -days XXX -nodes -subj $subj
+	openssl req -x509 -newkey rsa:2048 -keyout credentials/key.pem -out credentials/cert.pem -days XXX -nodes -subj $subj
 fi
+
+#############################
+# Enable service
+#############################
+read -p "Install and enable service [Y/n]: " response
+if [ "" = "$response" ] || [ "Y" = "$response" ] || [ "y" = "$response" ]; then
+	f=master-control-program.service
+	sudo systemctl disable 'master-control-program' > /dev/null 2>&1
+	echo "" > $f
+	echo "[Unit]" >> $f
+	echo "Description=Master Control Program" >> $f
+    echo "" >> $f
+	echo "[Service]" >> $f
+	echo "Type=oneshot" >> $f
+	echo "ExecStart=/usr/local/bin/node /home/pi/code/elecronicdoor/server/software/server-app.js" >> $f
+	echo "WorkingDirectory=`pwd`"
+    echo "" >> $f
+	echo "[Install]" >> $f
+	echo "WantedBy=multi-user.target" >> $f
+    echo "" >> $f
+
+	sudo systemctl enable `pwd`/$f
+fi
+
 echo ""
-echo "Done! Use 'node ./server-app.js' to run"
+echo "Done!"
+echo "    To start the service, use 'sudo systemctl start master-control-program"
+echo "    To run manually,      use 'sudo node ./server-app.js'"
