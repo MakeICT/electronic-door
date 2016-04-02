81ffb3af399a100847170ce673c90406f50b1bfa
Dominic Canare <dom@makeict.org>
2016-04-01 22:27:29 -0500

    create plugin

---
+++ server/software/plugins/mcp-plugin-journalctl/index.js
@@ -0,0 +1,68 @@
+var backend = require('../../backend.js');
+var child_process = require('child_process');
+var tmp = require('tmp');
+var fs = require('fs');
+
+var runningProcs = {};
+
+module.exports = {
+	name: 'Systemd Journal Viewer',
+	options: {
+		'Line limit': 'number'
+	},
+	actions: {
+		'Download': function(session){
+			backend.getPluginOptions(module.exports.name, function(settings){
+				tmp.file(function(err, tmpFilePath, fd, cleanupCallback){
+					var filename = encodeURIComponent(tmpFilePath.substring(tmpFilePath.lastIndexOf('/')+1));
+					session.response.send({ 'url': '/plugins/' + encodeURIComponent(module.exports.name) + '/handler?f=' + filename });
+
+					var proc = child_process.spawn('journalctl -u master-control-program --no-pager > ' + tmpFilePath);
+					runningProcs[filename] = {
+						'proc' : proc,
+						'running': true,
+						'cleanup': cleanupCallback,
+					};
+					proc.stdout.on('data', function(data) {
+						fs.appendFile(tmpFilePath, data);
+					});
+					proc.stdout.on('end', function(data){
+						backend.log('Systemd Log Generated');
+						backend.debug(filename);
+						runningProcs[filename].running = false;
+						fs.close(fd);
+					});
+					proc.stdin.end();
+				});
+			});
+		}
+	},
+
+	onInstall: function(){},
+	onUninstall: function(){},
+	onEnable: function(){},
+	onDisable: function(){},
+	
+	handleRequest: function(request, response){
+		var f = request.params.f;
+		if(!runningProcs[f]){
+			response.send(404, 'File not found :(');
+			response.end();
+		}else if(runningProcs[f].running){
+			response.write('<html><head><meta http-equiv="refresh" content="10" /></head><body><pre><h1>[' + (new Date()) + '] Retreiving log...</h1></pre></body></html>');
+			response.end();
+		}else{
+			fs.readFile('/tmp/' + f, 'utf8', function(err, file) {
+				if (err) {
+					backend.error('Systemd log failed :(');
+					response.send(500);
+				}else{
+					response.write(file);
+					runningProcs[f].cleanup();
+					delete runningProcs[f];
+				}
+				response.end();
+			});
+		}
+	},
+};
