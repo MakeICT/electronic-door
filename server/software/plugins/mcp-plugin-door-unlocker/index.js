97fdf4e2e2b182d41fd0c70caa18e9262bdd061c
Dominic Canare <dom@makeict.org>
2016-04-05 22:43:46 -0500

    fix error on unauth

--- server/software/plugins/mcp-plugin-door-unlocker/index.js
+++ server/software/plugins/mcp-plugin-door-unlocker/index.js
@@ -22,14 +22,14 @@ function fixUnlockDuration(duration){
 }
 
 // @param client can be client object or clientID
-function doUnlock(client, user, nfc){
+function doUnlock(client, userID, nfc){
 	try{
 		// regroup the options by key/value pairs for easy lookup
 		var options = backend.regroup(client.plugins[module.exports.name].options, 'name', 'value');
 
 		superSerial.send(client.clientID, superSerial.SERIAL_COMMANDS['UNLOCK'], fixUnlockDuration(options['Unlock duration']));
 		broadcaster.broadcast(module.exports, "door-unlocked", { 'client': client.clientID });
-		backend.log(client.name, user.userID, nfc, 'unlock');
+		backend.log(client.name, userID, nfc, 'unlock');
 	}catch(exc){
 		backend.error('Exception during unlock');
 		backend.error(exc);
@@ -142,7 +142,7 @@ module.exports = {
 					
 					var unlock = function(user){
 						// @TODO add user to log
-						doUnlock(client, user, nfc);
+						doUnlock(client, user.userID, nfc);
 					};
 					var deny = function(user){
 						var userID = (user == null) ? null : user.userID;
