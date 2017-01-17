8fe8401b999e803f572e3f3418dd556259d6a4e0
Dominic Canare <dom@makeict.org>
2017-01-16 19:22:47 -0600

    enable autoscroll in console

--- server/software/public/js/controllers/ConsoleController.js
+++ server/software/public/js/controllers/ConsoleController.js
@@ -1,9 +1,32 @@
 app.factory('consoleService', ["$rootScope", function($rootScope) {
     var consoleService = {
 		'messages': [],
+		'autoScroll': false,
+		'toggleScroll': function(forceMode){
+			if(forceMode === undefined){
+				forceMode = !(consoleService.autoScroll != false);
+			}
+			
+			if(forceMode){
+				consoleService.autoScroll = setInterval(
+					function(){
+						var consoleDiv = document.getElementById('message-log-list');
+						consoleDiv.scrollTop = consoleDiv.scrollHeight;
+					}, 500
+				);
+				
+				return true;
+			}else{
+				clearInterval(consoleService.autoScroll);
+				consoleService.autoScroll = false;
+				return false;
+			}
+
+		},
 		'addMessage': function(type, message){
 			var date = new Date();
 			var time = pad(date.getHours(), 2) + ':' + pad(date.getMinutes(), 2) + ':' + pad(date.getSeconds(), 2);
+			
 			consoleService.messages.push({type:type, text: message, timestamp: time});
 			$rootScope.$apply();
 			console.log(message);
@@ -32,5 +55,9 @@ app.controller('consoleCtrl', function($scope, consoleService, tunePlayer){
 
 	$scope.clearMessages = function(){
 		consoleService.messages.length = 0;
+	};
+	
+	$scope.toggleScroll = function(){
+		$scope.autoScrollEnabled = consoleService.toggleScroll();
 	};
 });
