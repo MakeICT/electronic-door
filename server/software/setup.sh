d7eb010f5187e1fbba31f8ef29ec3e385eb3def7
Dominic Canare <dom@makeict.org>
2017-07-20 01:58:18 -0500

    convert setup for python deps

--- server/software/setup.sh
+++ server/software/setup.sh
@@ -29,56 +29,38 @@ fi
 #############################
 # Node dependencies
 #############################
-read -p "Install node dependencies [Y/n]: " response
+read -p "Install Python dependencies [Y/n]: " response
 if [ "" = "$response" ] || [ "Y" = "$response" ] || [ "y" = "$response" ]; then
-	npm install
-	for plugin in plugins/*; do
-		cd $plugin
-		npm install
-		cd ../../
-	done
-	cd plugins/mcp-plugin-alarm-decoder/node_modules/ad2usb
-	npm install
-fi
-
-#############################
-# HTTPS cert and key
-#############################
-read -p "Generate new SSL cert and key [Y/n]: " response
-if [ "" = "$response" ] || [ "Y" = "$response" ] || [ "y" = "$response" ]; then
-	mkdir credentials >/dev/null 2>&1
-	subj="/CN=mcp.makeict.org/O=MakeICT/C=US/ST=Kansas/L=Wichita/OU=IT"
-
-	openssl req -x509 -newkey rsa:2048 -keyout credentials/key.pem -out credentials/cert.pem -days XXX -nodes -subj $subj
-	openssl req -x509 -newkey rsa:2048 -keyout credentials/key.pem -out credentials/cert.pem -days XXX -nodes -subj $subj
+	pip3 install flask flask_socketio eventlet bcrypt
+	sudo apt-get install libqt4-sql-psql
 fi
 
 #############################
 # Enable service
 #############################
-read -p "Install and enable service [Y/n]: " response
-if [ "" = "$response" ] || [ "Y" = "$response" ] || [ "y" = "$response" ]; then
-	f=master-control-program.service
-	sudo systemctl disable 'master-control-program' > /dev/null 2>&1
-	echo "
-		[Unit]
-		Description=Master Control Program
-		
-		[Service]
-		ExecStart=`which node` `pwd`/server-app.js
-		WorkingDirectory=`pwd`
-		Restart=always
-
-		[Install]
-		WantedBy=multi-user.target
-	" | awk '{$1=$1};1' > $f
-
-	sudo systemctl enable `pwd`/$f
-	echo ""
-	echo "    To start the service, use"
-	echo "        sudo systemctl start $f"
-	echo "    or reboot"
-	echo "    To run manually, use"
-	echo "        sudo node ./server-app.js"
-fi
+#read -p "Install and enable service [Y/n]: " response
+#if [ "" = "$response" ] || [ "Y" = "$response" ] || [ "y" = "$response" ]; then
+#	f=master-control-program.service
+#	sudo systemctl disable 'master-control-program' > /dev/null 2>&1
+#	echo "
+#		[Unit]
+#		Description=Master Control Program
+#		
+#		[Service]
+#		ExecStart=`which node` `pwd`/server-app.js
+#		WorkingDirectory=`pwd`
+#		Restart=always
+#
+#		[Install]
+#		WantedBy=multi-user.target
+#	" | awk '{$1=$1};1' > $f
+#
+#	sudo systemctl enable `pwd`/$f
+#	echo ""
+#	echo "    To start the service, use"
+#	echo "        sudo systemctl start $f"
+#	echo "    or reboot"
+#	echo "    To run manually, use"
+#	echo "        sudo node ./server-app.js"
+#fi
 echo "Done!"
