2fb950715e35eec8ca0cd38049c268c74c364fb7
Christian Kindel <iceman81292@gmail.com>
2019-07-26 09:25:02 -0500

    remove unnecessary pages and layout items, update bootstrap and other external dependencies

--- mcp/main/routes.py
+++ mcp/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, Blueprint
+from flask import render_template, request, redirect, url_for, Blueprint
 from mcp.models import Post
 
 main = Blueprint('main', __name__)
@@ -9,6 +9,7 @@ main = Blueprint('main', __name__)
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return redirect(url_for('users.account'))
     return render_template('home.html', posts=posts)
 
 
